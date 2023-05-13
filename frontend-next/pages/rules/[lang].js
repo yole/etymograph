cbd735d4480b719576bbba67aeff40cbec4870cb
Dmitry Jemerov <intelliyole@gmail.com>
2023-05-13 09:59:57 +0200

    next.js frontend: rule list page

---
+++ frontend-next/pages/rules/[lang].js
@@ -0,0 +1,34 @@
+import {useEffect} from "react";
+import {fetchAllLanguagePaths, fetchBackend} from "@/api";
+import Link from "next/link";
+import {useRouter} from "next/router";
+
+export async function getStaticProps(context) {
+    return fetchBackend(`rules/${context.params.lang}`)
+}
+
+export async function getStaticPaths() {
+    return await fetchAllLanguagePaths();
+}
+
+export default function RuleList(params) {
+    const ruleList = params.loaderData
+    const router = useRouter()
+    useEffect(() => { document.title = "Etymograph : Rules" })
+
+    return <>
+        <h2>
+            <small>
+                <Link href={`/`}>Etymograph</Link> {'> '}
+                <Link href={`/language/${router.query.lang}`}>{ruleList.toLangFullName}</Link> &gt; </small>
+            Rules
+        </h2>
+        {ruleList.ruleGroups.map(g => <>
+            <h2 key={g.groupName}>{g.groupName}</h2>
+            <ul>
+                {g.rules.map(r => <li key={r.id}><Link href={`/rule/${r.id}`}>{r.name}</Link>{r.summaryText.length > 0 ? ": " + r.summaryText : ""}</li>)}
+            </ul>
+        </>)}
+        <Link href="/rules/new">Add rule</Link>
+    </>
+}
