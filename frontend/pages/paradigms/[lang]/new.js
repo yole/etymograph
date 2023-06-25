9e8bc8b03e0dcf27e7ebf8eaa150c192ba8178ef
Dmitry Jemerov <intelliyole@gmail.com>
2023-06-25 21:53:50 +0200

    Error handling for new paradigm

--- frontend/pages/paradigms/[lang]/new.js
+++ frontend/pages/paradigms/[lang]/new.js
@@ -6,12 +6,19 @@ export default function ParadigmEditor() {
     const [name, setName] = useState("")
     const [pos, setPos] = useState("")
     const [editableText, setEditableText] = useState("")
+    const [errorText, setErrorText] = useState("")
     const router = useRouter()
 
     function saveParadigm() {
         addParadigm(name, router.query.lang, pos, editableText)
-            .then(r => r.json())
-            .then(r => router.push("/paradigm/" + r.id))
+            .then(r => {
+                if (r.status === 200) {
+                    r.json().then(r => router.push("/paradigm/" + r.id))
+                }
+                else {
+                    r.json().then(r => setErrorText(r.message.length > 0 ? r.message : "Failed to save paradigm"))
+                }
+            })
     }
 
     return <>
@@ -27,6 +34,7 @@ export default function ParadigmEditor() {
         </tbody></table>
         <textarea rows="10" cols="50" value={editableText} onChange={e => setEditableText(e.target.value)}/>
         <br/>
+        {errorText !== "" && <div className="errorText">{errorText}</div>}
         <button onClick={saveParadigm}>Save</button>
     </>
 }
