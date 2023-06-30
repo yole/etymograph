a62399b5d63f2090f430ce20b7dcf1510ec019c2
Dmitry Jemerov <intelliyole@gmail.com>
2023-06-30 18:13:07 +0200

    Include variations in the list of attestations

--- model/src/main/kotlin/InMemoryGraphRepository.kt
+++ model/src/main/kotlin/InMemoryGraphRepository.kt
@@ -123,7 +123,7 @@ open class InMemoryGraphRepository : GraphRepository() {
         val result = mutableSetOf<Word>()
         result.add(word)
         for (link in getLinksTo(word)) {
-            if (link.fromEntity is Word && (link.type == Link.Derived || link.type == Link.Agglutination)) {
+            if (link.fromEntity is Word && (link.type == Link.Derived || link.type == Link.Agglutination || link.type == Link.Variation)) {
                 result.add(link.fromEntity)
             }
         }
