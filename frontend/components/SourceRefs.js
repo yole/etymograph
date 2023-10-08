c068b3d78b9fea6a6128c2e5864d6822efe00770
Dmitry Jemerov <intelliyole@gmail.com>
2023-10-08 16:49:54 +0200

    Render commas between multiple sources

--- frontend/components/SourceRefs.js
+++ frontend/components/SourceRefs.js
@@ -7,15 +7,16 @@ export default function SourceRefs(props) {
     }
 
     return <div className="source">Source:{' '}
-        {source.map(s => {
+        {source.map((s, index) => {
+            const prefix = index > 0 ? ", " : ""
             if (s.pubId !== null) {
-                return <><Link href={`/publication/${s.pubId}`}>{s.pubRefId}</Link>{":" + s.refText}</>
+                return <>{prefix}<Link href={`/publication/${s.pubId}`}>{s.pubRefId}</Link>{":" + s.refText}</>
             }
             else if (s.refText.startsWith("http")) {
-                return <a href={s.refText}>{s.refText}</a>
+                return <>{prefix}<a href={s.refText}>{s.refText}</a></>
             }
             else {
-                return s.refText
+                return prefix + s.refText
             }
         })}
     </div>
