017148c1fd50895d61adc432f30805e848160c8d
Dmitry Jemerov <intelliyole@gmail.com>
2024-03-18 17:34:51 +0100

    Correctly fix error in findLink

--- web/src/test/kotlin/LinkControllerTest.kt
+++ web/src/test/kotlin/LinkControllerTest.kt
@@ -50,4 +50,30 @@ class LinkControllerTest {
         val linkViewModel = ruleViewModel.linkedWords.single()
         assertEquals("abc", linkViewModel.toWord.text)
     }
+
+    @Test
+    fun linkRuleFromBase() {
+        val fixture = QTestFixture()
+        val ruleController = RuleController(fixture.graphService)
+        ruleController.newRule(
+            RuleController.UpdateRuleParameters(
+                "q-pos",
+                "q", "q",
+                "sound is 't' and end of word:\n- new sound is 'θ'",
+            ))
+
+        val linkController = LinkController(fixture.graphService)
+        val baseWord = fixture.graph.findOrAddWord("mbar", fixture.ce, "home")
+        val derivedWord = fixture.graph.findOrAddWord("mar", fixture.q, "home")
+        val link = fixture.graph.addLink(derivedWord, baseWord, Link.Derived, emptyList(), emptyList(), null)
+
+        linkController.updateLink(LinkController.LinkParams(
+            fromEntity = baseWord.id,
+            toEntity = derivedWord.id,
+            linkType = Link.Derived.id,
+            ruleNames = "q-pos"
+        ))
+
+        assertEquals("q-pos", link.rules.single().name)
+    }
 }
