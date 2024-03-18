5a2c4b2296e33e687f620d11b9a48f238b00bb86
Dmitry Jemerov <intelliyole@gmail.com>
2024-03-18 16:13:11 +0100

    Breadcrumbs in 'new rule sequence' page

--- frontend/pages/rules/sequence/new.js
+++ frontend/pages/rules/sequence/new.js
@@ -1,5 +1,6 @@
 import RuleSequenceForm from "@/components/RuleSequenceForm";
 import {useRouter} from "next/router";
+import Link from "next/link";
 
 export default function RuleSequenceEditor() {
     const router = useRouter()
@@ -8,5 +9,13 @@ export default function RuleSequenceEditor() {
         router.push("/rules/" + toLang)
     }
 
-    return <RuleSequenceForm submitted={submitted}/>
+    return <>
+        <h2>
+            <small>
+                <Link href={`/`}>Etymograph</Link> {'> '}
+            </small>
+            New Rule Sequence
+        </h2>
+        <RuleSequenceForm submitted={submitted}/>
+    </>
 }
