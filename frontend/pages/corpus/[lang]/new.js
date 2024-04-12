cf33ddd6ec6f33bada161941c019841b4a0fe0aa
Dmitry Jemerov <intelliyole@gmail.com>
2024-04-12 13:56:47 +0200

    Breadcrumbs in 'new corpus text' form

--- frontend/pages/corpus/[lang]/new.js
+++ frontend/pages/corpus/[lang]/new.js
@@ -1,7 +1,18 @@
 import {useRouter} from "next/router";
 import CorpusTextForm from "@/components/CorpusTextForm";
+import {fetchAllLanguagePaths, fetchBackend} from "@/api";
+import Link from "next/link";
 
-export default function CorpusTextEditor() {
+export async function getStaticProps(context) {
+    return fetchBackend(`language/${context.params.lang}`)
+}
+
+export async function getStaticPaths() {
+    return await fetchAllLanguagePaths();
+}
+
+export default function CorpusTextEditor(params) {
+    const langData = params.loaderData
     const router = useRouter()
 
     const lang = router.query.lang
@@ -10,5 +21,15 @@ export default function CorpusTextEditor() {
         router.push("/corpus/text/" + r.id)
     }
 
-    return <CorpusTextForm lang={lang} submitted={submitted}/>
+    return <>
+        <h2>
+            <small>
+                <Link href={`/`}>Etymograph</Link> {'> '}
+                <Link href={`/language/${lang}`}>{langData.name}</Link> {'> '}
+            </small>
+            New Corpus Text
+        </h2>
+
+        <CorpusTextForm lang={lang} submitted={submitted}/>
+    </>
 }
