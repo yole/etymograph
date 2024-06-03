23538665d85d372845cea27800e5dedc467c709a
Dmitry Jemerov <intelliyole@gmail.com>
2024-06-03 18:39:06 +0200

    Fix one more linter error preventing publication

--- frontend/components/WordLink.js
+++ frontend/components/WordLink.js
@@ -7,13 +7,14 @@ export default function WordLink(params) {
     const graph = router.query.graph
 
     const word = params.word
-    let linkTarget = `/${graph}/word/${word.language}/${word.text.toLowerCase()}`;
+    let linkTarget = `/${graph}/word/${word.language}/${word.text.toLowerCase()}`
     if (word.homonym) {
         linkTarget += `/${word.id}`
     }
+    const wordText = word.reconstructed ? ("*" + word.text) : word.text
     return <>
         {params.baseLanguage !== undefined && word.language !== params.baseLanguage && word.displayLanguage + " "}
-        <Link href={linkTarget}>{word.reconstructed && "*"}{word.text}</Link>
+        <Link href={linkTarget}>{wordText}</Link>
         {params.gloss && word.gloss !== null && <>{' '}"<WordGloss gloss={word.gloss}/>"</>}
     </>
 }
