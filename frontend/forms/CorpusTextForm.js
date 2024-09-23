82b326450c22c791816f2dbd3e3eaf407581cab2
Dmitry Jemerov <yole@hey.com>
2024-09-23 10:34:08 +0200

    Enable input assist for corpus text form

--- frontend/forms/CorpusTextForm.js
+++ frontend/forms/CorpusTextForm.js
@@ -15,7 +15,7 @@ export default function CorpusTextForm(props) {
         <table><tbody>
             <FormRow id="title" label="Title"/>
         </tbody></table>
-        <FormTextArea rows="10" cols="50" id="text"/>
+        <FormTextArea rows="10" cols="50" id="text" inputAssist={true}/>
         <table>
             <tbody>
             <FormRow id="source" label="Source" />
