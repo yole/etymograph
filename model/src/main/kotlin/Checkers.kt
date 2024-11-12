0bfd4a38b20f2bc81dace29e72a097b3fe0bfe61
Dmitry Jemerov <yole@hey.com>
2024-11-12 12:20:18 +0100

    Gloss checker

--- model/src/main/kotlin/Checkers.kt
+++ model/src/main/kotlin/Checkers.kt
@@ -19,10 +19,30 @@ object PosChecker : ConsistencyChecker {
             {
                 report(ConsistencyCheckerIssue("No POS specified for word $word"))
             }
+            else if (word.pos != null && language.pos.none { it.abbreviation == word.pos }) {
+                report(ConsistencyCheckerIssue("POS not in list for word $word"))
+            }
+        }
+    }
+}
+
+object GlossChecker : ConsistencyChecker {
+    override fun check(repo: GraphRepository, language: Language, report: (ConsistencyCheckerIssue) -> Unit) {
+        for (word in repo.allWords(language)) {
+            val baseWordLink = word.baseWordLink(repo)
+            val variationOf = word.getVariationOf(repo)
+            if ((baseWordLink != null || variationOf != null) && word.gloss != null) {
+                report(ConsistencyCheckerIssue("Gloss specified for derived word $word"))
+            }
+            else if (baseWordLink == null && variationOf == null && word.gloss == null &&
+                word.pos != KnownPartsOfSpeech.properName.abbreviation && repo.findCompoundsByCompoundWord(word).isEmpty())
+            {
+                report(ConsistencyCheckerIssue("No gloss specified for word $word"))
+            }
         }
     }
 }
 
 val consistencyCheckers = listOf(
-    PosChecker
+    PosChecker, GlossChecker
 )
