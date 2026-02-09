d710aa616c0a5b7da3f8bfe016fa149ba6166388
Dmitry Jemerov <yole@hey.com>
2026-02-09 10:38:25 +0100

    Render subscripts in syllabogram names

--- frontend/components/WordTextView.tsx
+++ frontend/components/WordTextView.tsx
@@ -6,11 +6,20 @@ function SyllabogramView(params: {syllabogram: Syllabogram, prevSyllabogram?: Sy
     const delimiter = !params.prevSyllabogram || syl.type == "Determinative" || params.prevSyllabogram.type == "Determinative"
         ? ""
         : (syl.type == "Logogram" ? "." : "-")
+
+    const renderTextWithSubscript = (text: string) => {
+        const match = text.match(/^(\D+)(\d+)$/)
+        if (match && match[1]) {
+            return <>{match[1]}<sub>{match[2]}</sub></>
+        }
+        return <>{text}</>
+    }
+
     return <>
         {delimiter}
         {syl.type == "LogogramAlt" && <i>{syl.text}</i>}
         {syl.type == "Determinative" && <sup>{syl.text}</sup>}
-        {syl.type != "LogogramAlt" && syl.type != "Determinative" && <>{syl.text}</>}
+        {syl.type != "LogogramAlt" && syl.type != "Determinative" && <>{renderTextWithSubscript(syl.text)}</>}
     </>
 }
 
