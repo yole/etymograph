3d841facf662f57b3e72491de728d02015dd4023
Dmitry Jemerov <yole@hey.com>
2026-07-20 17:18:06 +0200

    Exclude search page from readonly export

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -5,6 +5,10 @@ const nextConfig = {
 
 if (process.env.NEXT_PUBLIC_READONLY === "true") {
   nextConfig.output = "export"
+  nextConfig.exportPathMap = async (defaultPathMap) =>
+    Object.fromEntries(Object.entries(defaultPathMap).filter(
+      ([, entry]) => entry.page !== "/[graph]/search"
+    ))
 }
 
 module.exports = nextConfig
